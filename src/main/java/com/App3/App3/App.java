9864a977bdd456ddb34f944e2c256f9c582c94a9
Dhage <priyanka-popat.dhage@capgemini.com>
2023-09-02 11:26:08 +0530

    new data added

---
+++ src/main/java/com/App3/App3/App.java
@@ -0,0 +1,13 @@
+package com.App3.App3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
